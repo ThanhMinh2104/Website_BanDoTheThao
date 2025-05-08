b701efb61f1c8e49e832af9111fc006c2233e286
Nguyen Ho Thanh Minh <nghothminz@gmail.com>
2025-05-08 20:43:37 +0700

    7. Add Overlay layout when click btn search in header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,10 +3,13 @@ import styles from "./Header.module.scss";
 import images from "~/assests/images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faMagnifyingGlass, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [showOverlay, setShowOverlay] = useState(false);
+
     return (
         <header className={cx("wrapper")}>
             <div className={cx("inner")}>
@@ -15,7 +18,11 @@ function Header() {
                     <img className={cx("logo-image")} src={images.logo} alt="logo"></img>
                 </div>
                 <div className={cx("actions")}>
-                    <FontAwesomeIcon className={cx("action-icon")} icon={faMagnifyingGlass} />
+                    <FontAwesomeIcon
+                        className={cx("action-icon")}
+                        icon={faMagnifyingGlass}
+                        onClick={() => setShowOverlay(true)}
+                    />
                     <FontAwesomeIcon className={cx("action-icon")} icon={faUser} />
                     <div className={cx("cart")}>
                         <span className={cx("cart-total")}>0đ</span>
@@ -23,6 +30,19 @@ function Header() {
                     </div>
                 </div>
             </div>
+
+            {/* Overlay và nội dung */}
+            {showOverlay && (
+                <>
+                    <div className={cx("overlay")} onClick={() => setShowOverlay(false)}></div>
+                    <div className={cx("search-wrapper")}>
+                        <input type="text" className={cx("search-input")} placeholder="Tìm kiếm sản phẩm..." />
+                        <i className={cx("search-icon")}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={() => {}} />
+                        </i>
+                    </div>
+                </>
+            )}
         </header>
     );
 }
