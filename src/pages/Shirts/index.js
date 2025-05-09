bee35589c0872f5722c00941c0fe4f6363352bf9
Nguyen Ho Thanh Minh <nghothminz@gmail.com>
2025-05-09 15:52:40 +0700

    14.Update remove 3 links

--- src/pages/Shirts/index.js
+++ src/pages/Shirts/index.js
@@ -1,7 +1,3 @@
-import styles from "./Shirts.module.scss";
-import classNames from "classnames/bind";
-import ShirtItem from "~/components/Layout/components/ShirtItem";
-
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
@@ -13,7 +9,7 @@ const cx = classNames.bind(styles);
 function Shirts() {
     const { clubId } = useParams();
     const [shirts, setShirts] = useState([]);
-    
+
     useEffect(() => {
         fetch("http://localhost:3001/shirts")
             .then((response) => response.json())
