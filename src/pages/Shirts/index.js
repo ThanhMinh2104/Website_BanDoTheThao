273454947cfe38825fa6ca6e364a9aeaabe22a25
ntanloi <nguyenloidt.052017@gmail.com>
2025-05-09 13:11:50 +0700

    11. Create Shirts in db.json, update Shirts use useEffect, update ShirtItem

--- src/pages/Shirts/index.js
+++ src/pages/Shirts/index.js
@@ -1,7 +1,7 @@
 import styles from "./Shirts.module.scss";
 import classNames from "classnames/bind";
-import images from "~/assests/images";  
 import ShirtItem from "~/components/Layout/components/ShirtItem";
+<<<<<<< Updated upstream
 
 const cx = classNames.bind(styles);
 
@@ -54,6 +54,44 @@ function Shirts() {
         <div className={cx("wrapper")}>
             <div className={cx("grid")}>
                 {mancity.map((item) => (
+=======
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const cx = classNames.bind(styles);
+
+function Shirts() {
+    const { clubId } = useParams();
+    const [shirts, setShirts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/shirts")
+            .then((response) => response.json())
+            .then((data) => {
+                const clubShirts = data[clubId]; // Lấy mảng áo đấu theo clubId
+                if (clubShirts) {
+                    // Nếu ảnh dùng "~", thay thế bằng đúng đường dẫn từ public/
+                    const normalizedShirts = clubShirts.map((shirt) => ({
+                        ...shirt,
+                        image1: shirt.image1.replace(/^~\/assets/, "/assets"),
+                        image2: shirt.image2.replace(/^~\/assets/, "/assets"),
+                    }));
+                    setShirts(normalizedShirts);
+                } else {
+                    setShirts([]);
+                }
+            })
+            .catch((error) => {
+                console.error("Lỗi khi fetch áo đấu:", error);
+                setShirts([]);
+            });
+    }, [clubId]);
+
+    return (
+        <div className={cx("wrapper")}>
+            <div className={cx("grid")}>
+                {shirts.map((item) => (
+>>>>>>> Stashed changes
                     <ShirtItem
                         key={item.id}
                         shirtName={item.shirtName}
